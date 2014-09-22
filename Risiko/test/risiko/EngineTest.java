55d3740a3a44c0ede1f11ddb6e5e133573473f26
xilaew@gmail.com <xilaew@gmail.com>
2014-09-22 23:23:29 +0200

    Implemented Engine reading and writing XMI files.

---
+++ Risiko/test/risiko/EngineTest.java
@@ -0,0 +1,18 @@
+package risiko;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class EngineTest {
+
+	public static void main(String[] args) throws FileNotFoundException,
+			IOException, RuntimeException {
+		Engine engine = new Engine();
+		File boardFile = new File("examples/board.board");
+		engine.setBoard(new FileInputStream(boardFile));
+		engine.getBoard(System.out);
+	}
+
+}
